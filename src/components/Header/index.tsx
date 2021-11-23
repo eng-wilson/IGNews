4972e24734d66caa0bb5d6770413056de1c68b89
Wilson Carvalho <wilson@fuerzastudio.com.br>
2021-11-22 23:28:19 -0300

    style: home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" width={0} height={0} />
+        <Image src="/images/logo.svg" alt="ig.news" width={100} height={50} />
 
         <nav>
           <a href="" className={styles.active}>
