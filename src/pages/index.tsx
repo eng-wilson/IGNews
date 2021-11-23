bc1ca23a2df1652f718f11be4b07abae17b57d98
Wilson Carvalho <wilson@fuerzastudio.com.br>
2021-11-23 14:07:27 -0300

    chore: get product price added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
+import { GetServerSideProps } from "next";
+
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
+      {console.log(product)}
       <Head>
         <title>Home | ig.news</title>
       </Head>
@@ -21,10 +32,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -37,3 +48,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Jz1vQA313HMRUyhJ3IrxlM7");
+
+  console.log(price);
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+  };
+};
