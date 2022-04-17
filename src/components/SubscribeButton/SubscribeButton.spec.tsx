0ddbdce58efa5eaa7c1fdb623db408a97a83bf03
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-04-17 20:04:31 -0300

    feat: add subscription button tests

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,65 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { mocked } from "jest-mock";
+import NextAuth, { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { SubscribeButton } from ".";
+
+jest.mock("next-auth/react");
+jest.mock("next/router");
+
+describe("SubscribeButton component", () => {
+  const useSessionMocked = mocked(useSession);
+  it("should render correctly", () => {
+    useSessionMocked.mockReturnValueOnce({ data: null, status: "loading" });
+
+    render(<SubscribeButton />);
+
+    expect(screen.getByText("Subscribe now")).toBeInTheDocument();
+  });
+
+  it("should redirect user to sign in when not logged in", () => {
+    const signInMocked = mocked(signIn);
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({ data: null, status: "loading" });
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText("Subscribe now");
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it("should redirect user to posts when user already has subscription", () => {
+    const useSessionMocked = mocked(useSession);
+    const useRouterMocked = mocked(useRouter);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          name: "John Doe",
+          email: "johndoe@example.com",
+        },
+        activeSubscription: "fake-active-subscription",
+        expires: "fake-expires",
+      },
+      status: "authenticated",
+    });
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText("Subscribe now");
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMock).toHaveBeenCalled();
+  });
+});
